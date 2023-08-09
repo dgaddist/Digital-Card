55cb973587996f10cad0bab4c9d9f19a4786a3fc
dgaddist <128024246+dgaddist@users.noreply.github.com>
2023-08-08 17:36:19 -0700

    Center card image to the middle of page and resize img

--- Card.js
+++ Card.js
@@ -1,14 +1,14 @@
-import React from "react"
-import Body from "./components/Body"
-import Footer from "./components/Footer"
-import Nav from "./components/Nav"
+import React from "react";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
 
 export default function Card() {
-    return (
-        <div>
-        <Nav />
-        <Body />
-        <Footer />
-        </div>
-    )
+  return (
+    <div className="card-contents">
+      <Nav />
+      <Body />
+      <Footer />
+    </div>
+  );
 }
